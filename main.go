d51f83e82a0e400f025c6f66c0df2d6ce188503d
magodo <wztdyl@sina.com>
2020-11-09 11:18:50 +0800

    simplified

--- main.go
+++ main.go
@@ -2,10 +2,9 @@ package main
 
 import (
 	"fmt"
-	"io"
+	"go/ast"
 	"io/ioutil"
 	"os"
-	"strings"
 
 	"github.com/hashicorp/hcl2/hcl"
 	"github.com/hashicorp/hcl2/hcl/hclsyntax"
@@ -21,46 +20,14 @@ func main() {
 
 	src, err := ioutil.ReadFile(fname)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "cannot read file: %v", err)
+		fmt.Fprintf(os.Stderr, "cannot read file: %v\n", err)
 		os.Exit(1)
 	}
 
-	file, diags := hclsyntax.ParseConfig(src, fname, hcl.InitialPos)
+	f, diags := hclsyntax.ParseConfig(src, fname, hcl.InitialPos)
 	if len(diags) != 0 {
-		fmt.Fprintln(os.Stderr, "failed to parse: %v", diags.Error())
+		fmt.Fprintf(os.Stderr, "failed to parse: %v\n", diags.Error())
 		os.Exit(1)
 	}
-
-	walker := &Walker{0, os.Stdout}
-
-	// The body in the returned file has dynamic type *hclsyntax.Body, so callers may freely
-	// type-assert this to get access to the full hclsyntax API in situations where detailed
-	// access is required.
-	// https://pkg.go.dev/github.com/hashicorp/hcl2/hcl/hclsyntax#ParseConfig
-	diags = hclsyntax.Walk(file.Body.(*hclsyntax.Body), walker)
-	if len(diags) != 0 {
-		fmt.Fprintln(os.Stderr, "failed to walk the ast: %v", diags.Error())
-		os.Exit(1)
-	}
-}
-
-type Walker struct {
-	indent int
-	writer io.Writer
-}
-
-func (w *Walker) Enter(node hclsyntax.Node) hcl.Diagnostics {
-	w.indent++
-	switch node := node.(type) {
-	case *hclsyntax.Attribute:
-		// TODO
-		w.writer.Write([]byte(strings.Repeat(" ", w.indent) + node.Name + "\n"))
-	}
-	// TODO
-	return nil
-}
-
-func (w *Walker) Exit(node hclsyntax.Node) hcl.Diagnostics {
-	w.indent--
-	return nil
+	ast.Print(nil, f)
 }
